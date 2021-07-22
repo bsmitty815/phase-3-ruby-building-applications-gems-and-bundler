7b7f2a7e4091a669989cd342c4a5743224aa1b96
Brian Smith <brian.bubbam@gmail.com>
2021-07-22 15:53:46 -0500

    commit tree 2bb51a717c01dec40003308d53aa4b62fe8a4ca9

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,7 @@
 # Set up Bundler here following the instructions in the README
+
+#to install gem
+#put this in terminal - gem install rspec 
+
+require 'bundler/setup'
+Bundler.require(:default, :development) 
